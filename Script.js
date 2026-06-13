824f937349572133b8e3d4a674cabc5e9f8918dc
angelinafink <angelina1.fink@gmail.com>
2026-06-13 11:50:47 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,6 +1,8 @@
 const uebergabe = document.getElementById("uebergabe");
 const adresseBereich = document.getElementById("adresseBereich");
 
+const vereinsPLZ = "49448";
+
 uebergabe.addEventListener("change", funktion () {
 
       if (uebergabe.value === "Abholung durch Sammelfahrzeug") {
@@ -9,3 +11,17 @@ uebergabe.addEventListener("change", funktion () {
           adresseBereich.style.display = "none";
       }
   });
+
+document.querySelector("form").addEventListener("submit", function (event)
+    event.preventDefault();
+
+    const plz = document.getElementById("plz").value;
+
+    if (uebergabe.value === "Abholung durch Sammelfahrzeug") {
+          if (plz.substring(0,2) !== vereinsPLZ.substring(0, 2)) {
+              alert("Die Abholadresse liegt leider nicht in der Nähe der Geschäftsstelle.");
+              return;
+            }
+          }
+          alert("Die Registrierung war erfolgreich.");
+ });
