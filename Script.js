01392dab615516b610a2bce1d3c89c2cf7b0862a
angelinafink <angelina1.fink@gmail.com>
2026-06-13 11:42:24 +0200

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,11 @@
-console.log("Kleiderspenden-Portal gestartet");
+const uebergabe = document.getElementById("uebergabe");
+const adresseBereich = document.getElementById("adresseBereich");
+
+uebergabe.addEventListener("change", funktion () {
+
+      if (uebergabe.value === "Abholung durch Sammelfahrzeug") {
+          adresseBereich.style.display = "block";
+      } else {
+          adresseBereich.style.display = "none";
+      }
+  });
